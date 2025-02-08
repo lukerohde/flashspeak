207e273d3e17d25b956e76844e64195d2e3e136f
Luke Rohde <lukerohde@gmail.com>
2025-02-08 19:33:45 +1100

    Split out API

--- app/voice_chat/urls.py
+++ app/voice_chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from . import api
 
 app_name = 'voice_chat'
 
 urlpatterns = [
     path('', views.VoiceChatView.as_view(), name='index'),
-    path('api/session/', views.SessionAPIView.as_view(), name='session'),
+    path('api/session/', api.SessionAPIView.as_view(), name='session'),
 ]
